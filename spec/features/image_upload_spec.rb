c276ead5856d519e526396c6e1695083de66fa9a
Victor <victorpearson89@gmail.com>
2014-10-23 14:25:28 -0700

    Created test for image uploading

---
+++ spec/features/image_upload_spec.rb
@@ -0,0 +1,17 @@
+require "features/helper"
+
+feature "Image Upload Spec" do
+  let(:user_hash) { Fixtures::Users[:registered] }
+  let(:user) { User.create(user_hash) }
+  scenario "A logged in user may upload a picture" do
+    login_as(user)
+    click_link_or_button "upload_picture"
+
+    attach_file "picture_file", Fixtures.image_path(some-image.png)
+
+    fill_in "picture_caption", with: "A cool picture"
+    fill_in "picture_description", with: "Words on words"
+
+    click_link_or_button "commit"
+  end
+end
